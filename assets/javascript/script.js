7fec6db48d5229ec86540385403773db478737fd
Preeti Gupta <preetigupta510@gmail.com>
2020-07-08 19:17:48 -0700

    adding layout

---
+++ assets/javascript/script.js
@@ -0,0 +1,28 @@
+function getDocHeight(doc) {
+    doc = doc || document;
+    // stackoverflow.com/questions/1145850/
+    var body = doc.body, html = doc.documentElement;
+    var height = Math.max( body.scrollHeight, body.offsetHeight, 
+        html.clientHeight, html.scrollHeight, html.offsetHeight );
+    return height;
+}
+
+function setIframeHeight() {
+    var ifrm = $("#contentFrame");
+    var doc = ifrm.contentDocument? ifrm.contentDocument: 
+        ifrm.contentWindow.document;
+    ifrm.style.visibility = 'hidden';
+    ifrm.style.height = "10px"; // reset to minimal height ...
+    // IE opt. for bing/msn needs a bit added or scrollbar appears
+    ifrm.style.height = getDocHeight( doc ) + 4 + "px";
+    ifrm.style.visibility = 'visible';
+}
+
+$(".navigation").on('click',showPage);
+$("#contentFrame").on('load',setIframeHeight);
+
+function showPage(){
+    var url = $(this).attr('value');
+    $('#contentFrame').attr('src', url);
+    setIframeHeight();
+}
