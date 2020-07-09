6303e33e5cfddb8a0bd18e678ec5e3a87f6ab921
Preeti Gupta <preetigupta510@gmail.com>
2020-07-08 21:36:33 -0700

    finishing protofoli

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,21 +8,28 @@ function getDocHeight(doc) {
 }
 
 function setIframeHeight() {
-    var ifrm = $("#contentFrame");
+    var ifrm = document.getElementById("contentFrame");
+    console.log(ifrm.contentDocument);
+    console.log(ifrm.contentWindow);
     var doc = ifrm.contentDocument? ifrm.contentDocument: 
         ifrm.contentWindow.document;
     ifrm.style.visibility = 'hidden';
     ifrm.style.height = "10px"; // reset to minimal height ...
     // IE opt. for bing/msn needs a bit added or scrollbar appears
-    ifrm.style.height = getDocHeight( doc ) + 4 + "px";
+    console.log(getDocHeight( doc ));
+    ifrm.style.height = getDocHeight( doc ) + 200 + "px";
+    console.log(ifrm.style.height);
     ifrm.style.visibility = 'visible';
 }
 
 $(".navigation").on('click',showPage);
-$("#contentFrame").on('load',setIframeHeight);
-
+//$("#contentFrame").on('load',setIframeHeight);
+$('.collapse').on('click',toggle);
+function toggle()
+{
+    alert("Hi");
+}
 function showPage(){
     var url = $(this).attr('value');
     $('#contentFrame').attr('src', url);
-    setIframeHeight();
 }
